b9587a73f5df6d0fa0e746698447f2e048f97629
phat-nguyen-alephon-com <phat.nguyen@alephon.com>
2024-10-23 08:17:59 +0700

    start success

--- config.php
+++ config.php
@@ -35,7 +35,8 @@ $CFG->dboptions = array (
 );
 
 // change when deploy //
-$CFG->wwwroot   = 'http://localhost:8080/moodle4113';
+#$CFG->wwwroot   = 'http://ec2-52-77-228-9.ap-southeast-1.compute.amazonaws.com';
+$CFG->wwwroot   = 'http://localhost:8002';
 $CFG->dataroot  = __DIR__.'/data/moodledata4113';
 $CFG->admin     = 'admin';
 
