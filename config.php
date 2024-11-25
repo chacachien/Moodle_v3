d807e33feb3c8da495f4c810a2c89dc8c9be5a85
phat-nguyen-alephon-com <phat.nguyen@alephon.com>
2024-11-25 13:43:27 +0700

    done ui improve

--- config.php
+++ config.php
@@ -36,14 +36,13 @@ $CFG->dboptions = array (
 
 // change when deploy //
 #CFG->wwwroot   = 'http://ec2-54-254-161-50.ap-southeast-1.compute.amazonaws.com';
-$CFG->wwwroot   = 'http://localhost:8080/moodle4113';
+$CFG->wwwroot   = 'http://localhost:8002';
 $CFG->dataroot  = __DIR__.'/data/moodledata4113';
 $CFG->admin     = 'admin';
 
 $CFG->directorypermissions = 0777;
 
 require_once(__DIR__ . '/lib/setup.php');
-
 
 $CFG->cachejs = false;
 $CFG->debug = DEBUG_DEVELOPER;
